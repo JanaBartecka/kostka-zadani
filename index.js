cc16bee8daf880c1bb1fd68054bf3c8c8b3a756b
Jana Bartecka <xcrazygirl@seznam.cz>
2023-03-12 21:55:08 +0100

    20230312 hotovo

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('funguju!');
+
+const Img=document.querySelector('.dice')
+let side=1
+
+document.addEventListener('keypress', () => {
+    let ImgSrc=Img.getAttribute('src')
+    let ImageNumber=ImgSrc.slice(8,9)
+    if (side===6) {
+        side=1
+    } else {
+        side=side+1
+    }
+    Img.src="img/side" + side + ".svg"
+})
+
